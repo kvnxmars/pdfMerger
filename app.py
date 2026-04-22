9b9d64b2b32a8f8d81bbdfb3de2d0667933f5c72
kvnxmars <bookingskevinmars@gmail.com>
2026-04-22 16:59:32 +0200

    changed ports

--- app.py
+++ app.py
@@ -89,4 +89,5 @@ def merge_pdfs():
         print(f"Error during merging: {str(e)}")
         return jsonify({'error': str(e)}), 500
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
